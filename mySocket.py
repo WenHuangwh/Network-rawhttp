1fdaa39018d18504cf61436d3b407ed043675983
wenhuang1 <huangwen@login.khoury.neu.edu>
2023-03-16 22:07:53 -0400

    receive finshed?

--- mySocket.py
+++ mySocket.py
@@ -376,7 +376,7 @@ class RawSocket:
             
             total_payload = b''.join(received_data)
 
-            print(f'current lenght of recv {len(total_payload) / 1024 / 1024}')
+            # print(f'current lenght of recv {len(total_payload) / 1024 / 1024}')
 
         total_payload = b''.join(received_data)
         header, _, body = total_payload.partition(b'\r\n\r\n')
