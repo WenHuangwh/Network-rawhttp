3be437958fe479895e6a866e9d3ac84747673a55
wenhuang1 <huangwen@login.khoury.neu.edu>
2023-03-17 13:11:54 -0400

    receive finshed?

---
+++ compare.py
@@ -0,0 +1,24 @@
+def compare_bytes(a, b):
+    print(f"len MB: {len(a)}, len OK: {len(b)}")
+
+    min_length = min(len(a), len(b))
+    
+    for i in range(min_length):
+        if a[i] != b[i]:
+            print(f"diff at index {i}, MB = {a[i]}, OK = {b[i]}")
+    
+    return 
+
+
+
+def main():
+    with open('10MB.log', 'rb') as f:
+        file_1_contents = f.read()
+
+    with open('10OK.log', 'rb') as f:
+        file_2_contents = f.read()
+
+    differences = compare_bytes(file_1_contents, file_2_contents)
+
+
+main()
