af690c0a7fff7cb7878c9082b914360525c44deb
wenhuang1 <huangwen@login.khoury.neu.edu>
2023-03-18 18:07:48 -0400

    test send

--- mySocket.py
+++ mySocket.py
@@ -154,7 +154,7 @@ class RawSocket:
                 if self._seq not in buffer:
                     packet_number_to_send = i
                     break
-                data = buffer[self.seq]
+                data = buffer[self._seq]
                 self._seq = seq_to_send
                 self._send_one(flags=PSH_ACK, data=data)
                 self._seq += len(data)
